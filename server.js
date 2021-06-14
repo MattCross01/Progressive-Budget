b09c224bfad935e653d8cb2bf46f96b097f05c5e
MattCross01 <mountaindew2141@outlook.com>
2021-06-13 20:03:56 -0400

    added server.js and index.html

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express=require("express")
+logger=require("morgan")
+mongoose=require("mongoose")
+compression=require("compression")
 
-const PORT = process.env.PORT || 3000;
+PORT=process.env.PORT||3e3,app=express();
 
-const app = express();
-
-app.use(logger("dev"));
-
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use(logger("dev")),
+app.use(compression()),
+app.use(express.urlencoded({extended:!0})),
+app.use(express.json()),
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
-mongoose.connect(MONGODB_URI);
+var MONGODB_URI=process.env.MONGODB_URI||"mongodb://localhost/budget";
 
-app.use(require("./routes/api.js"));
+mongoose.connect(MONGODB_URI),
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+app.use(require("./routes/api.js")),
+app.listen(PORT,
+    ()=>{console.log(`App running on port ${PORT}!`)});
+    
